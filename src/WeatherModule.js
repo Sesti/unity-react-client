9cd56932bd8014e4069ba000b05d2897a4bae0c9
Sébastian Pilon <sebastian.pilon@gmail.com>
2019-09-13 17:26:42 -0400

    Initial commit

---
+++ src/WeatherModule.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import WeatherIcon from 'react-icons-weather';
+import "./WeatherModule.css";
+
+class WeatherModule extends Component {
+	
+	constructor(props){
+		super( props );
+		this.state = {
+			data: {
+				temperature : 0,
+				temperatureMin : "",
+                temperatureMax : "",
+                humidity : "",
+                sunrise : "",
+                sunset : "",
+				weatherId : "",
+                weatherIcon : "",
+                weatherDescription : "",
+                weatherMain : "",
+                windDegrees : "",
+                windSpeed : "",
+				city : "",
+			}
+		}
+	}
+	
+	fetchData(){
+		const context = this;
+		fetch( "http://localhost:3000/api/v1/weather", {method: 'get'} )
+			.then( function ( response ){
+				return response.json();
+			} )
+			.then( function ( json ){
+                console.log(json);
+                let obj = {
+                    temperature : json.main.temp,
+                    temperatureMin : json.main.temp_min,
+                    temperatureMax : json.main.temp_max,
+                    humidity : json.main.humidity,
+                    sunrise : json.sys.sunrise,
+                    sunset : json.sys.sunset,
+                    weatherId : json.weather[0].id,
+                    weatherIcon : json.weather[0].icon,
+                    weatherDescription : json.weather[0].description,
+                    weatherMain : json.weather[0].main,
+                    windDegrees : json.wind.deg,
+                    windSpeed : json.wind.speed,
+                    city : json.name,
+                }
+
+				context.setState( {data: obj} );
+			} );
+	}
+	
+	tick(){
+		this.fetchData();
+	}
+	
+	componentDidMount(){
+		this.fetchData();
+		this.timerID = setInterval(
+			() => this.tick(),
+			60000
+		);
+	}
+	
+	componentWillUnmount(){
+		clearInterval(this.timerID);
+	}
+	
+	render(){
+        const {temperature, temperatureMin, temperatureMax, humidity, sunrise, sunset, weatherId, weatherIcon, 
+            weatherDescription, weatherMain, windDegrees, windSpeed, city } = this.state.data;
+		
+		return (
+            <div className="App-widget">
+				<WeatherIcon name="owm" className="weather-icon" iconId="200"/>
+				<div>{Math.round(temperatureMin)}&#176;</div>
+				<div>{Math.round(temperature)}&#176;</div>
+				<div>{Math.round(temperatureMax)}&#176;</div>
+                <div>{humidity}</div>
+                <div>{sunrise}</div>
+                <div>{sunset}</div>
+                <div>{weatherDescription}</div>
+                <div>{weatherMain}</div>
+                <div>{windDegrees}</div>
+                <div>{windSpeed}</div>
+				<div>{city}</div>
+            </div>
+		);
+	}
+}
+
+export default WeatherModule;
